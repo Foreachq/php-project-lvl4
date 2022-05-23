a7e091ca5f70ba4aa83c02ec66d377668dfc5ed5
Ilya Golovin <foreachq@gmail.com>
2022-05-23 18:33:05 +0300

    fix: move lang from session to own cookie

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cookie;
 
 class HomeController extends Controller
 {
+    private const FIVE_YEARS_IN_MIN = 5 * 365 * 24 * 60;
+
     public function index(): Renderable
     {
         return view('home');
@@ -16,7 +19,7 @@ class HomeController extends Controller
     public function changeLang($lang): RedirectResponse
     {
         App::setLocale($lang);
-        session()->put('lang', $lang);
+        Cookie::queue('lang', $lang, self::FIVE_YEARS_IN_MIN);
 
         return redirect()->back();
     }
--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cookie;
 
 class Language
 {
@@ -16,13 +17,11 @@ class Language
      * @param Request $request
      * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return Response|RedirectResponse
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
      */
     public function handle(Request $request, Closure $next): Response|RedirectResponse
     {
-        if (session()->has('lang')) {
-            App::setLocale(session()->get('lang'));
+        if (Cookie::has('lang')) {
+            App::setLocale(Cookie::get('lang'));
         }
 
         return $next($request);
