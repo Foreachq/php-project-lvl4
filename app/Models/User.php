5d6b0f8571be60720dc9f2e59b979653a12bea8a
Ilya Golovin <foreachq@gmail.com>
2022-05-25 19:53:37 +0300

    add: Task model and relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -28,4 +28,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function createdTasks(): HasMany
+    {
+        return $this->hasMany('App\Models\Task', 'created_by_id');
+    }
+
+    public function executingTasks(): HasMany
+    {
+        return $this->hasMany('App\Models\Task', 'assigned_to_id');
+    }
 }
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,14 +3,13 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
-class TaskStatusFactory extends Factory
+class TaskFactory extends Factory
 {
     public function definition(): array
     {
         return [
-            'name' => Str::random(7),
+            //
         ];
     }
 }
