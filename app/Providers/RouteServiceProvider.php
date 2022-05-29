587be36b3f0bd31fb905529c6232f666a80a2d3e
Ilya Golovin <foreachq@gmail.com>
2022-05-30 00:49:22 +0300

    fix: routing patterns

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,6 +36,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        $this->pattern('task', '[0-9]+');
+        $this->pattern('task_status', '[0-9]+');
+        $this->pattern('label', '[0-9]+');
     }
 
     /**
